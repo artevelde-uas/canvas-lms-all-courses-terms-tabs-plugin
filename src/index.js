21e4d5761d36d85287f5a99b655c73a85d2e429a
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2023-04-18 22:55:10 +0200

    Move function outside main function

--- src/index.js
+++ src/index.js
@@ -2,52 +2,51 @@ import { router, dom } from '@artevelde-uas/canvas-lms-app';
 import __ from './i18n';
 
 
-export default function () {
+function fixCourseTable(el) {
+    const rows = Array.from(el.querySelectorAll('tbody > tr'));
+    const cells = Array.from(el.querySelectorAll('td.course-list-term-column'));
+    const terms = cells.map(el => el.innerText.trim()).filter((value, index, self) => (self.indexOf(value) === index));
+    const tabs = document.createElement('div');
 
-    function fixCourseTable(el) {
-        const rows = Array.from(el.querySelectorAll('tbody > tr'));
-        const cells = Array.from(el.querySelectorAll('td.course-list-term-column'));
-        const terms = cells.map(el => el.innerText.trim()).filter((value, index, self) => (self.indexOf(value) === index));
-        const tabs = document.createElement('div');
-
-        tabs.classList.add('ui-tabs-minimal', 'ui-tabs', 'ui-widget', 'ui-widget-content', 'ui-corner-all');
-        tabs.innerHTML = `
-            <ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all" role="tablist">
-                <li class="ui-state-default ui-corner-top ui-tabs-active ui-state-active" role="tab">
-                    <a class="ui-tabs-anchor" role="presentation">${__('all_terms')}</a>
+    tabs.classList.add('ui-tabs-minimal', 'ui-tabs', 'ui-widget', 'ui-widget-content', 'ui-corner-all');
+    tabs.innerHTML = `
+        <ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all" role="tablist">
+            <li class="ui-state-default ui-corner-top ui-tabs-active ui-state-active" role="tab">
+                <a class="ui-tabs-anchor" role="presentation">${__('all_terms')}</a>
+            </li>
+            ${terms.map(term => `
+                <li class="ui-state-default ui-corner-top" role="tab">
+                    <a class="ui-tabs-anchor" role="presentation">${term}</a>
                 </li>
-                ${terms.map(term => `
-                    <li class="ui-state-default ui-corner-top" role="tab">
-                        <a class="ui-tabs-anchor" role="presentation">${term}</a>
-                    </li>
-                `).join('')}
-            </ul>
-        `;
+            `).join('')}
+        </ul>
+    `;
 
-        tabs.addEventListener('click', e => {
-            const tab = e.target.closest('li');
+    tabs.addEventListener('click', e => {
+        const tab = e.target.closest('li');
 
-            if (tab === null) return;
+        if (tab === null) return;
 
-            tabs.querySelector('.ui-tabs-active').classList.remove('ui-tabs-active', 'ui-state-active');
-            tab.classList.add('ui-tabs-active', 'ui-state-active');
+        tabs.querySelector('.ui-tabs-active').classList.remove('ui-tabs-active', 'ui-state-active');
+        tab.classList.add('ui-tabs-active', 'ui-state-active');
 
-            rows.forEach(row => {
-                const cell = row.querySelector('td.course-list-term-column');
+        rows.forEach(row => {
+            const cell = row.querySelector('td.course-list-term-column');
 
-                if (cell === null) return;
+            if (cell === null) return;
 
-                if (cell.innerText.trim() === tab.innerText || tab.innerText === __('all_terms')) {
-                    row.removeAttribute('hidden');
-                } else {
-                    row.setAttribute('hidden', 'hidden');
-                }
-            });
+            if (cell.innerText.trim() === tab.innerText || tab.innerText === __('all_terms')) {
+                row.removeAttribute('hidden');
+            } else {
+                row.setAttribute('hidden', 'hidden');
+            }
         });
+    });
 
-        el.parentNode.insertBefore(tabs, el);
-    }
+    el.parentNode.insertBefore(tabs, el);
+}
 
+export default function () {
     router.onRoute('courses', () => {
         dom.onElementAdded('#my_courses_table', fixCourseTable);
         dom.onElementAdded('#past_enrollments_table', fixCourseTable);
